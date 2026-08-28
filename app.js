ea56a1f01d77b06fe53bcb38c809b54630ae4af2
Alireza <alireza.khorramshekooh@gmail.com>
2026-08-28 23:15:24 +0800

    refactore routes

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 200,
     results: tours.length,
     data: { tours },
   });
-});
+};
 
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTourById = (req, res) => {
   const { id } = req.params;
   const tour = tours.find((el) => el.id === +id);
 
@@ -31,9 +31,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: { tour },
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
@@ -49,7 +49,11 @@ app.post('/api/v1/tours', (req, res) => {
       });
     },
   );
-});
+};
+
+app.get('/api/v1/tours', getAllTours);
+app.get('/api/v1/tours/:id', getTourById);
+app.post('/api/v1/tours', createTour);
 
 const port = 3000;
 
