313635f1c515903a456a3590e476d1107f6335f0
Alireza <alireza.khorramshekooh@gmail.com>
2026-09-19 16:00:08 +0330

    feat: connect create tour endpoint to database

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,15 +1,15 @@
 const Tour = require('../models/tourModel');
 
 //Middleware to Check body data
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price!',
-    });
-  }
-  next();
-};
+// exports.checkBody = (req, res, next) => {
+//   if (!req.body.name || !req.body.price) {
+//     return res.status(400).json({
+//       status: 'fail',
+//       message: 'Missing name or price!',
+//     });
+//   }
+//   next();
+// };
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
@@ -30,14 +30,22 @@ exports.getTourById = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
-  // const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  // tours.push(newTour);
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour: newTour,
-    },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({});
+    // newTour.save()
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
 };
