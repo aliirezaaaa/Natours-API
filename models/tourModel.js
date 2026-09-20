a34b0ec1a429f3dca07c4b031318a98d29e18e50
Alireza <alireza.khorramshekooh@gmail.com>
2026-09-20 20:47:39 +0330

    feat: add aggregation middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -87,6 +87,11 @@ tourSchema.pre(/^find/, function () {
   this.find({ secretTour: { $ne: true } });
 });
 
+// Aggregation Middleware. here 'this' keyword points to current aggregation object.
+tourSchema.pre('aggregate', function () {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
