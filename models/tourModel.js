0905cf4fda086777290f96b8001ef6dfed9385ed
Alireza <alireza.khorramshekooh@gmail.com>
2026-09-20 20:05:51 +0330

    fix: change Date.now() to Date.now

--- models/tourModel.js
+++ models/tourModel.js
@@ -53,7 +53,7 @@ const tourSchema = new mongoose.Schema(
     images: [String],
     createdAt: {
       type: Date,
-      default: Date.now(),
+      default: Date.now,
     },
     startDates: [Date],
   },
