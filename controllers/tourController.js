d09acd05b7ffe362e0cfb6f5afcabb9181ac0ec2
Alireza <alireza.khorramshekooh@gmail.com>
2026-09-20 11:20:55 +0330

    feat: add pagination to get tour endpoint

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -40,6 +40,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    //5) Pagination
+    //In order to convert string to number, I multiply it by 1. If the query parameter is not provided, I set a default value.
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
